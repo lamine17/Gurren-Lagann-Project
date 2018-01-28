17ca1a8265940d4f81a5a05f54bce69bda0918fc
ilyesbb <ilyes.boussama@yahoo.fr>
2018-01-28 22:06:08 +0000

    creation de l'arene et des objets qui la composents

---
+++ TestPoint.py
@@ -0,0 +1,12 @@
+from Point import *
+
+#Modification/acces des attributs
+A=Point(0,0,0)
+B=Point(1,2,3)
+print(A)
+A.setPosition(Point(1,2,3))
+A.deplacer(Point(0,1,0))
+print(A)
+
+print("coordonnee y: {}".format(A.getCoordonnees()[1]))
+print("Attribut inexistant: {}".format(A.att))
