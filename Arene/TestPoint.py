66902c017992ab3b6cc701b1a9c94616a3c86882
bezalielmarvel <bezalielmarvel@yahoo.com>
2018-01-30 21:18:46 +0100

    ADD ARENE

--- Arene/TestPoint.py
+++ Arene/TestPoint.py
@@ -3,7 +3,7 @@ from Point import *
 #Modification acces des attributs
 A=Point(0,0,0)
 B=Point(1,2,3)
-print("a = ".format(A))
+#print("a = ".format(A))
 A.setPosition(Point(0,0,1))
 A.deplacer(Point(0,1,0))
 print("deplacement de a = {}".format(A))
