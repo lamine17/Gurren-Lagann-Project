cd8b1a14ef5a0fa969a2fe9589f52ea69af11d87
ilyesbb <ilyes.boussama@yahoo.fr>
2018-01-30 17:31:15 +0000

    la norme d'un vecteur est controlée grâce à un attribut qui stocke l'erreur

--- TestVecteur.py
+++ TestVecteur.py
@@ -1,19 +1,18 @@
 from Vecteur import *
 
 #operations sur les vecteurs
-v2=Vecteur(1,0,0)
-v1=Vecteur(300,0,0)
+v1=Vecteur(347,450,200)
+v2=Vecteur(1000,0,0)
 print("v1 = {}, v2 = {}".format(v1, v2))
 print("v1 + v2 = {}".format(v1+v2))
 print("v1 * v2 = {}".format(v1*v2))
 print("v1 ^ v2 = {}".format(v1**v2))
 
 #rotation
-n=8
+n=10
 teta=2*pi/n
-horizontale=Vecteur(1,0,0)
-diff=v1.diffAngle2D(horizontale)
-print("angle : {} radians / {} degres ".format(diff,int(diff*360/(2*pi))))
+angle=v1.getAngle2D()
+print("angle : {} radians / {} degres ".format(angle,int(angle*360/(2*pi))))
 print("v1 = {}".format(v1))
 
 print("{} rotation de {} radians/ {} degres de v1 = {}".format(n,teta, int(teta*360/(2*pi)), v1))
@@ -23,7 +22,7 @@ for i in range(0, n):
     print("rotation de v1...")
     v1.rotation2D(teta)
     print("|v1|= |{}| = {}".format(v1,v1.getNorme()))
-    diff=v1.diffAngle2D(horizontale)
-    print("angle : {} radians / {} degres ".format(diff,int(diff*360/(2*pi))))
+    angle=v1.getAngle2D()
+    print("angle : {} radians / {} degres ".format(angle,int(angle*360/(2*pi))))
 
 
