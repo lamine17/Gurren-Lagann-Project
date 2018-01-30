f1ac1b5e536bdbddbad126f00c6fbafe239828e8
ilyesbb <ilyes.boussama@yahoo.fr>
2018-01-30 12:51:47 +0000

    ajout d'une methode pour mesurer l'angle entre 2 vecteurs en 2D

--- TestVecteur.py
+++ TestVecteur.py
@@ -1,8 +1,8 @@
 from Vecteur import *
 
 #operations sur les vecteurs
-v1=Vecteur(1,0,0)
-v2=Vecteur(3,646,23)
+v2=Vecteur(1,0,0)
+v1=Vecteur(3,0,23)
 print("v1 = {}, v2 = {}".format(v1, v2))
 print("v1 + v2 = {}".format(v1+v2))
 print("v1 * v2 = {}".format(v1*v2))
@@ -11,6 +11,19 @@ print("v1 ^ v2 = {}".format(v1**v2))
 #rotation
 n=8
 teta=2*pi/n
+horizontale=Vecteur(1,0,0)
+diff=v1.diffAngle2D(horizontale)
+print("angle : {} radians / {} degres ".format(diff,int(diff*360/(2*pi))))
+print("v1 = {}".format(v1))
+
+print("{} rotation de {} radians/ {} degres de v1 = {}".format(n,teta, int(teta*360/(2*pi)), v1))
+
 for i in range(0, n):
+    print("|v1|= |{}| = {}".format(v1,v1.getNorme()))
+    print("rotation de v1...")
     v1.rotation2D(teta)
-print("{} rotation de {} radians de v1 = {}".format(n,teta, v1))
+    print("|v1|= |{}| = {}".format(v1,v1.getNorme()))
+    diff=v1.diffAngle2D(horizontale)
+    print("angle : {} radians / {} degres ".format(diff,int(diff*360/(2*pi))))
+
+
--- Vecteur.py
+++ Vecteur.py
@@ -1,5 +1,7 @@
 from Point import *
 from math import *
+from fonctions import atan2
+
 class Vecteur(Point):
     """Defini des methodes de calcul sur les vecteurs """
     def __mul__(self, vecteur):
@@ -17,17 +19,23 @@ class Vecteur(Point):
         if issubclass(type(vecteur), Vecteur) and vecteur:
             return Vecteur(self._x+vecteur._x, self._y+vecteur._y, self._z+vecteur._z)
 
-    def diffAngle(self, vecteur):
+    def getAngle2D(self):
+        """ Retourne l'angle du vecteur par rapport a la verticale, dans le sens trigo"""
+        return atan2(self._y,self._x)
+    
+    def diffAngle2D(self, vecteur):
+        """retourne la difference d'angle entre 2 vecteurs dans le repere (x, y) """
         if issubclass(type(vecteur), Vecteur) and vecteur:
+            v=self**vecteur
             if self._y != 0 and vecteur._x != 0:
-                s=(self._x*vecteur._y)/(self._y*vecteur._x)
-                if s<0:
+                if v._z<0:
                     return -acos((self*vecteur)/(self.getNorme()*vecteur.getNorme()))
-                elif s>0:
+                elif v._z>0:
                     return acos((self*vecteur)/(self.getNorme()*vecteur.getNorme()))
                 else:
                     return 0
-            
+            return vecteur.getAngle2D()-self.getAngle2D()
+                
     def getNorme(self):
         """Retourne la norme du vecteur """
         return sqrt(pow(self._x,2)+pow(self._y,2)+pow(self._z,2))
@@ -35,8 +43,7 @@ class Vecteur(Point):
     def rotation2D(self, teta):
         """tourne le vecteur d'un angle teta"""
         x=self._x
-        print(self.getNorme())
         self._x=(int)(x*cos(teta)-self._y*sin(teta))
         self._y=(int)(x*sin(teta)+self._y*cos(teta))
-        print(self.getNorme())
+        
         
